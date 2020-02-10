fb586d65b9ea025354f929269defc134b23099ba
Donald-Brown <donbrown4652@gmail.com>
2020-02-10 10:39:28 -0600

    completes pig latin challenge

--- small_solutions/pig_latin/app.js
+++ small_solutions/pig_latin/app.js
@@ -1,7 +1,13 @@
 function translatePigLatin(str) {
-    let regex = /^[^aeiou]+/;
-    let newConsonants = str.match(regex);
-    return newConsonants;
+    if(str.match(/^[aeiou]/)){
+        return str.concat('way');
+    } else {
+        const consonants = str.match(/^[^aeiou]+/)[0];
+        //return consonants;
+        return str.substring(consonants.length).concat(consonants.concat('ay'));
+    }
 }
 console.log(translatePigLatin("rhythm"));
 console.log(translatePigLatin("glove"));
+
+console.log(translatePigLatin("osupercalifragilistic"));
