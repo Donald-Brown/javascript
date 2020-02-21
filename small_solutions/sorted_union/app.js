e6cfa17f29ca8bfd690ab4b6f1d76b378ed3dd6c
Donald Brown <donbrown4652@gmail.com>
2020-02-21 07:19:47 -0600

    adds sorted union

--- small_solutions/sorted_union/app.js
+++ small_solutions/sorted_union/app.js
@@ -1,9 +1,16 @@
-function uniteUnique(...args) {
-    console.log(...args);
-
-    let newARR;
-    
-    
-  }
+function uniteUnique(args) {
   
-  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+  let array = [...arguments];
+  let newArr = [];
+
+  for(let i=0; i<array.length; i++){
+    for(let j=0; j<array[i].length; j++){
+      if(!newArr.includes(array[i][j])){
+        newArr.push(array[i][j]);
+      }
+    }
+  }   
+  return newArr;
+}
+  
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
