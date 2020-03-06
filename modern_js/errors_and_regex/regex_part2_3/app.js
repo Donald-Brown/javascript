94efed93d10d017d78535f8f6b87cd70be0f8f2a
Donald Brown <donbrown4652@gmail.com>
2020-03-06 05:15:12 -0600

    more regex work

--- modern_js/errors_and_regex/regex_part2_3/app.js
+++ modern_js/errors_and_regex/regex_part2_3/app.js
@@ -16,9 +16,23 @@ re = /gre?a?y/; // ? optional character
 
 re = /gre?a?y\?/; // \ escape character
 
+// Brackets [] - Character sets
+re = /gr[ae]y/i // must be a or e
+re = /gr[^ae]y/i // matches anything except a or e
+re = /gr[a-z]y/i // matches any lowercase letter
+
+// braces {} equals quantifier
+re = /hel{2}o/; // must occur exactly {n} times
+re = /hel{2,4}o/; // must occur 2-4 times
+re = /hel{2,}o/; // must occur at least {n} times
+
+// Parenthasis are used for grouping
+re = /([0-9]x){3}/;
 
 // string to match 
-const str = 'gray?';
+const str = '3x3x3x';
+
+
 
 //log results
 const result = re.exec(str);
