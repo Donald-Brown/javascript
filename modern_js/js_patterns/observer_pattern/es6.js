41b73818c53c09238e25c0af98a1818c2bb686f3
Donald Brown <donbrown4652@gmail.com>
2020-03-08 07:48:21 -0500

    adds factory and observer patterns

---
+++ modern_js/js_patterns/observer_pattern/es6.js
@@ -0,0 +1,79 @@
+class EventObserver{
+    constructor(){
+        this.observers = [];
+    }
+
+    subscribe(fn){
+        this.observers.push(fn);
+        console.log(`You are now subscribed to ${fn.name}`);
+    }
+
+    unsubscribe(fn){
+        // Fiter out from the list whatever matches the callback function. If there is no such match, the callback gets to stay on the list. The returns a new list and reassigns the list of observers.
+        this.observers = this.observers.filter(function(item){
+            if(item !== fn){
+                return item;
+            }
+        });
+        console.log(`You are now unsubscibed from ${fn.name}`);
+    }
+
+    fire(){
+        this.observers.forEach(function(item){
+            item.call();
+        });
+    }
+}
+/*
+EventObserver.prototype = {
+    subscribe: function(fn){
+        this.observers.push(fn);
+        console.log(`You are now subscribed to ${fn.name}`);
+    },
+    unsubscribe: function(fn){
+        // Fiter out from the list whatever matches the callback function. If there is no such match, the callback gets to stay on the list. The returns a new list and reassigns the list of observers.
+        this.observers = this.observers.filter(function(item){
+            if(item !== fn){
+                return item;
+            }
+        });
+        console.log(`You are now unsubscibed from ${fn.name}`);
+    },
+    fire: function(){
+        this.observers.forEach(function(item){
+            item.call();
+        });
+    }
+}
+*/
+const click = new EventObserver();
+
+// Event Listeners
+document.querySelector('.sub-ms').addEventListener('click', function(){
+    click.subscribe(getCurMilliseconds);
+});
+
+document.querySelector('.unsub-ms').addEventListener('click', function(){
+    click.unsubscribe(getCurMilliseconds);
+});
+
+document.querySelector('.sub-s').addEventListener('click', function(){
+    click.subscribe(getCurSeconds);
+});
+
+document.querySelector('.unsub-s').addEventListener('click', function(){
+    click.unsubscribe(getCurSeconds);
+});
+
+document.querySelector('.fire').addEventListener('click', function(){
+    click.fire();
+});
+
+// click handlers
+const getCurMilliseconds = function(){
+    console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+}
+
+const getCurSeconds = function(){
+    console.log(`Current Seconds: ${new Date().getSeconds()}`);
+}
