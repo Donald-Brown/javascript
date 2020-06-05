4b419e4cbed73286701a60457f7fc9d9a9564162
Donald Brown <donbrown4652@gmail.com>
2020-06-05 12:13:38 -0500

    completes chapter 5 text

--- eloquent_js/examples/ch5/app.js
+++ eloquent_js/examples/ch5/app.js
@@ -140,4 +140,73 @@ for (let script of SCRIPTS) {
 //console.log(Math.round(total / count));
 // → 1165
 
+// the following usees the .some method to determine if the char code is contained a script and return it
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (
+      script.ranges.some(([from, to]) => {
+        return code >= from && code < to;
+      })
+    ) {
+      return script;
+    }
+  }
+  return null;
+}
+//console.log(characterScript(121));
+// → {name: "Latin", …}
 
+//the following deals with double coded characters such as emoji
+// Two emoji characters, horse and shoe
+let horseShoe = "🐴👟";
+// console.log(horseShoe.length);
+// // → 4
+// console.log(horseShoe[0]);
+// // → (Invalid half-character)
+// console.log(horseShoe.charCodeAt(0));
+// // → 55357 (Code of the half-character)
+// console.log(horseShoe.codePointAt(0));
+// // → 128052 (Actual code for horse emoji)
+
+// for/ of loops will give the character whichever it is
+let roseDragon = "🌹🐉";
+for (let char of roseDragon) {
+  //console.log(char);
+}
+// → 🌹
+// → 🐉
+
+// Please help me
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex(c => c.name == name);
+    if (known == -1) {
+      counts.push({name, count: 1});
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+//console.log(countBy([1, 2, 3, 4, 5], n => n > 2));
+// → [{name: false, count: 2}, {name: true, count: 3}]
+
+// Somehow the previous causes this to work
+function textScripts(text) {
+  let scripts = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.name : "none";
+  }).filter(({name}) => name != "none");
+
+  let total = scripts.reduce((n, {count}) => n + count, 0);
+  if (total == 0) return "No scripts found";
+
+  return scripts.map(({name, count}) => {
+    return `${Math.round(count * 100 / total)}% ${name}`;
+  }).join(", ");
+}
+
+//console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
+// → 61% Han, 22% Latin, 17% Cyrillic
